2a05611c47f72b37178673e75a884ec129a97a42
Philip Shanks <philip.shanks@gmail.com>
2016-07-27 11:12:27 -0700

    bring up to date

---
+++ examples/ex21.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+def add(a,b):
+    print "ADDING %d + %d" %(a,b)
+    return a + b
+
+def subtract(a,b):
+    print "SUBTRACTING %d - %d" %(a,b)
+    return a - b
+
+print add(3,4)
